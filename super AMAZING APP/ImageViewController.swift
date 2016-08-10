1ee1400403f13b68a6dc7194b63d685d01cd73e7
Anton Antonenko <aantonenko@jerasoft.net>
2016-08-10 18:39:09 +0300

    nn

--- super AMAZING APP/ImageViewController.swift
+++ super AMAZING APP/ImageViewController.swift
@@ -162,7 +162,7 @@ class ImageViewController: UIViewController, UICollectionViewDelegate, UIApplica
         cache.clearMemoryCache()
         cache.cleanExpiredDiskCache()
     }
-    let a = 2
+
     
     
     
